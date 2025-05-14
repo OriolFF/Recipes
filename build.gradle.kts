afa3083dee9ae76eb58a082fdc27d5c99bbd0828
Oriol Fernandez <oriol.developer@gmail.com>
2025-05-14 18:02:38 +0100

    Upgrade AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.9.2")
+        classpath("com.android.tools.build:gradle:8.10.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.51")
         classpath(libs.compose.compiler.gradle.plugin)
